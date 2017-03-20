f4bece0924ba3000a562bebb13a9e25f5d7c028c
Richa Sharma <Richa Sharma>
2017-03-20 19:11:51 +0530

    Done Manage profile and Change password in frontend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use Auth;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -23,7 +25,7 @@ class HomeController extends Controller
      * @return \Illuminate\Http\Response
      */
 
- public function index()
+    public function index()
     {
         // if(Auth::check()){
         //     echo "yes";
@@ -40,4 +42,36 @@ class HomeController extends Controller
             return view('home');
         }
     }    
+
+    public function profile_edit(Request $request){
+        $result=DB::table('users')
+                    ->where('id', Auth::id())
+                    ->update([
+                        'fname' => $request->input('fname'),
+                        'lname' => $request->input('lname'),
+                        'email' => $request->input('email'),
+                        'address' => $request->input('address'),
+                        ]);
+         return redirect('/home/my_profile')->with('success','Updations Successful');
+    }
+
+    public function change_user_password(Request $request){
+        if(Hash::check($request->input('old_password'),Auth::user()->password)){
+            $new_password=Hash::make($request->input('new_password'));
+
+            DB::table('users')
+                    ->where('id', Auth::id())
+                    ->update([
+                        'password' => $new_password,
+                        ]);
+            return redirect('/home/my_profile')->with('password_success','Password Updated Successful');
+        }
+        // $password=bcrypt($request->input('old_password'));
+        // if($password == Auth::user()->password){
+        //     echo "match";
+        // }else{
+        //     echo "not match";
+        // }
+        // print_r(Auth::user()->password);
+    }
 }
---
+++ resources/views/user/user_edit.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default animated bounce">
+                <div class="panel-heading"> Profile Editor </div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST"  enctype="multipart/form-data" action="/home/profile_edit">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('fname') ? ' has-error' : '' }}">
+                            <label for="fname" class="col-md-4 control-label">First Name</label>
+
+                            <div class="col-md-6">
+                                <input id="fname" type="text" class="form-control" name="fname" value="{{ Auth::user()->fname }}" required autofocus>
+
+                                @if ($errors->has('fname'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('fname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="lname" class="col-md-4 control-label">Last Name</label>
+
+                            <div class="col-md-6">
+                                <input id="lname" type="text" class="form-control" name="lname" value="{{ Auth::user()->lname }}" required autofocus>
+
+                                @if ($errors->has('lname'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('lname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ Auth::user()->email }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                         <div class="form-group">
+                            <label for="address" class="col-md-4 control-label">Address</label>
+
+                            <div class="col-md-6">
+                                <textarea name="address" class="form-control" placeholder="vill city " rows="4" required>{{ Auth::user()->address }}</textarea>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary" name="submit">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
